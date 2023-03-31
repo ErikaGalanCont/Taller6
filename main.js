cc5791b4a1b30c616b17c89c6b53ce8c83960e27
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-03-30 20:48:48 -0500

    ejercicio4

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-// 3.	Programa para solicitar el nombre, y la nota promedio de 5 estudiantes de un curso de computación.
-
-let estudiantes = [];
-
-for (let i = 0; i <= 4; i++) {
-
-    let nombre = prompt(`Ingrese el nombre del estudiante ${i}:`);
-    let nota = parseFloat(prompt(`Ingrese la nota promedio de ${nombre}:`));
-
-    estudiantes.push({ nombre, nota });
-}
-
-console.log(estudiantes);
-
+document.getElementById("convertir").addEventListener("click", function() {
+    let nota = document.getElementById("nota").value;
+    let equivalente = "";
+    if (nota >= 90 && nota <= 100) {
+        equivalente = "A";
+        document.getElementById("recomendacion").innerHTML = "Exceeds proficiency";
+    } else if (nota >= 80 && nota < 90) {
+        equivalente = "B";
+        document.getElementById("recomendacion").innerHTML = "Demonstrates proficiency";
+    } else if (nota >= 70 && nota < 80) {
+        equivalente = "C";
+        document.getElementById("recomendacion").innerHTML = "Approaches proficiency";
+    } else if (nota >= 60 && nota < 70) {
+        equivalente = "D";
+        document.getElementById("recomendacion").innerHTML = "Falls well below proficiency";
+    } else if (nota >= 0 && nota < 60) {
+        equivalente = "F";
+        document.getElementById("recomendacion").innerHTML = "Lacks all proficiency";
+    } else {
+        alert("Ingrese una nota válida");
+        return;
+    }
+});
